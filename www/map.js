84e003bca851e406e1ea01ddfb8c29f43108ee3b
Andrew Harvey <andrew.harvey4@gmail.com>
2015-07-05 13:40:26 +1000

    fix attribution

--- www/map.js
+++ www/map.js
@@ -21,6 +21,6 @@ var access_token = 'pk.eyJ1IjoiYW5kcmV3aGFydmV5NCIsImEiOiI5ZjY4N2YzNzI1N2U2ZGY5N
 /* use OSM as the base map */
 L.tileLayer('https://api.tiles.mapbox.com/v4/mapbox.light,alantgeo-presales.650e55dc/{z}/{x}/{y}@2x.png?access_token=' + access_token, {
         maxZoom: 18,
-        attribution: 'Base map <a href="https://www.mapbox.com/about/maps/" target="_blank">&copy; Mapbox</a> <a href="http://www.openstreetmap.org/about/" target="_blank">&copy; OpenStreetMap</a>; Overlay &copy; Sydney Water'
+        attribution: 'Base map <a href="https://www.mapbox.com/about/maps/" target="_blank">&copy; Mapbox</a> <a href="http://www.openstreetmap.org/about/" target="_blank">&copy; OpenStreetMap</a>; Overlay based on Australian Natural Hazards Data Archive &copy; Geoscience Australia CC BY 4.0'
         }).addTo(map);
 
